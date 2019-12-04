34416c7120e7487f847f12368f863c59cb65ef12
peterpeterparker <david.dalbusco@outlook.com>
2019-12-04 07:56:21 +0100

    feat: clean repo from dependencies and js build

--- sender/lib/filsat-api.d.ts
+++
@@ -1,7 +0,0 @@
-import { Task, TaskState } from './api';
-export declare class FilsatApi {
-    private graphQL;
-    getTasksInSubmission(): Promise<Task[]>;
-    private post;
-    updateTaskState(projectId: string, task: Task, state: TaskState): Promise<Task>;
-}
--- sender/lib/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const filsat_1 = require("./filsat");
-console.log('Filsat GitHub integration');
-(async () => {
-    const filsat = new filsat_1.Filsat();
-    await filsat.run();
-})();
